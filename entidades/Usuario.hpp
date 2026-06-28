206aa6e0ecd763d195950047508a5570761b1f73
TaZaiKo <estudiosjrchp@gmail.com>
2026-06-28 02:41:12 -0500

    first commit

---
+++ entidades/Usuario.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+class Usuario {
+private:
+    std::string username;
+    std::string password;
+    std::string email;
+    int idUsuario; 
+
+public:
+    Usuario() {} 
+    Usuario(std::string _username, std::string _password, std::string _email, int _id) 
+        : username(_username), password(_password), email(_email), idUsuario(_id) {}
+
+    // Getters básicos
+    std::string getUsername() const { return username; }
+    std::string getPassword() const { return password; }
+    std::string getEmail() const { return email; }
+    int getId() const { return idUsuario; }
+};
